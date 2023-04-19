7acfeaa8bbd35e460214fdfbd2d9f77d8bd8d10d
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2023-04-18 19:27:42 -0700

    Add files via upload

---
+++ EXCEPTION_HANDLING/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace EXCEPTION_HANDLING
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Pick a number to divide the array with.");
+                int divideNum = Convert.ToInt32(Console.ReadLine());
+                int[] numberList = { 12, 200, 100, 20, 30, 35};
+
+                foreach(int num in numberList)
+                {
+                    Console.WriteLine(num / divideNum);
+                }
+                Console.ReadKey();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
