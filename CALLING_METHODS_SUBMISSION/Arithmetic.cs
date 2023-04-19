32f9269814ac8dac57c39c2ce7b22088e1b64bed
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2023-04-19 12:15:57 -0700

    Add files via upload

---
+++ CALLING_METHODS_SUBMISSION/Arithmetic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALLING_METHODS_SUBMISSION
+{
+    class Arithmetic
+    {
+        public static int Add(int num)
+        {
+            int addTotal = num + 10;
+            return addTotal;
+        }
+
+        public static int Sub(int num)
+        {
+            int subTotal = num - 10;
+            return subTotal;
+        }
+
+        public static int Div(int num)
+        {
+            int DivTotal = num / 5;
+            return DivTotal;
+        }
+    }
+}
---
+++ CALLING_METHODS_SUBMISSION/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALLING_METHODS_SUBMISSION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number to perform an operation on.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Arithmetic.Add(input));
+            Console.WriteLine(Arithmetic.Sub(input));
+            Console.WriteLine(Arithmetic.Div(input));
+            Console.ReadKey();
+        }
+    }
+}
