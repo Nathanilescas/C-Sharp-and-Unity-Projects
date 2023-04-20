f1f6d129f87579ec80d18afe09bf96bf3762a896
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2023-04-20 13:24:11 -0700

    Add files via upload

---
+++ CLASS_SUBMISSION_ASSIGNMENT/DivOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_SUBMISSION_ASSIGNMENT
+{
+    class DivOperations
+    {
+        public void Div(int num1,out int num2)
+        {
+            num2 = num1 / 2;
+        }
+
+        public void Div(int num1)
+        {
+            num1 = (num1 / 2) - 1;
+        }
+
+        public static int MathOp(int num1)
+        {
+            num1 = (num1 * 10) / 2;
+            return num1;
+        }
+    }
+}
---
+++ CLASS_SUBMISSION_ASSIGNMENT/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_SUBMISSION_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DivOperations divBy2 = new DivOperations();
+
+            Console.WriteLine("Input a number to use on the method.");
+            int convertNumber = Convert.ToInt32(Console.ReadLine());
+
+            int output;
+            divBy2.Div(convertNumber,out output);
+            Console.WriteLine(output);
+
+
+            DivOperations divOp = new DivOperations();
+            divOp.Div(10);
+
+            DivOperations.MathOp(10);
+        }
+    }
+}
