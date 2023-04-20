263d2ea3a782e73f20999ec3bd0d12c527c269d1
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2023-04-20 11:54:32 -0700

    Add files via upload

---
+++ MAIN_METHOD_SUBMISSION/Arithmetic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIN_METHOD_SUBMISSION
+{
+    class Arithmetic
+    {
+        // MAIN METHOD SUBMISSION ASSIGNMENT //
+
+        public int Addition(int num)
+        {
+            int num2 = num + 10;
+            num2 = num2 + 2;
+            return num2;
+        }
+
+        public int Addition(decimal num)
+        {
+            num = num + 10;
+            int convertDec = Convert.ToInt32(num);
+            return convertDec;
+        }
+
+        public int Addition(string num)
+        {
+            int convertString = Convert.ToInt32(num);
+            int num2 = convertString + 15;
+            return num2;
+        }
+
+        ///////////////////////////////////////////////
+        /// METHOD SUBMISSION ASSIGNMENT #2 ///
+        public static int Operation(int num, int num2 = 0)
+        {
+            int total = num + num2;
+            return total;
+        }
+
+        ///////////////////////////////////////////////
+        /// METHOD CLASS SUBMISSION ASSIGNMENT # 3////
+        
+        public void Ops(int num1, int num2)
+        {
+            num1 = num1 *  10;
+            Console.WriteLine(num2);
+        }
+
+    }
+}
---
+++ MAIN_METHOD_SUBMISSION/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIN_METHOD_SUBMISSION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("METHOD CLASS SUBMISSION ASSIGNMENT #1");
+
+            Arithmetic firstOp = new Arithmetic();
+            Console.WriteLine(firstOp.Addition(5));
+
+            Arithmetic secondOp = new Arithmetic();
+            Console.WriteLine(secondOp.Addition(5.5m));
+
+            Arithmetic thirdOp = new Arithmetic();
+            Console.WriteLine(thirdOp.Addition("5"));
+
+
+            //////////////////////////////////////////////////
+            Console.WriteLine("METHOD SUBMISSION ASSIGNMENT #2");
+
+            Console.WriteLine("Input a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input a second number (Optional)");
+            string num2 = Console.ReadLine();
+            int convertNum2;
+
+            if (num2 != "")
+            {
+                convertNum2 = Convert.ToInt32(num2);
+                Console.WriteLine(Arithmetic.Operation(num1, convertNum2));
+            }
+            else
+            {
+                Console.WriteLine(Arithmetic.Operation(num1));
+            }
+
+
+            //////////////////////////////////////////////////////////
+            Console.WriteLine("METHOD CLASS SUBMISSION ASSIGNMENT #3"); 
+
+            Arithmetic methodOp = new Arithmetic();
+            methodOp.Ops(5, 10);
+            methodOp.Ops(num1: 10, num2: 20);
+
+            Console.ReadKey();
+        }
+    }
+}
