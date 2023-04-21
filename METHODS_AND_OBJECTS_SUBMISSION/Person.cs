223f172075093fee41add3d13818d06fa0f6dbd2
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2023-04-20 19:18:47 -0700

    Add files via upload

---
+++ METHODS_AND_OBJECTS_SUBMISSION/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHODS_AND_OBJECTS_SUBMISSION
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void SayName()
+        {
+            Console.WriteLine("Name : " + this.firstName + " " + this.lastName);
+        }
+    }
+}
---
+++ METHODS_AND_OBJECTS_SUBMISSION/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHODS_AND_OBJECTS_SUBMISSION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() {firstName = "Sample", lastName = "Student"};
+            employee1.SayName();
+            Console.ReadKey();
+        }
+    }
+}
