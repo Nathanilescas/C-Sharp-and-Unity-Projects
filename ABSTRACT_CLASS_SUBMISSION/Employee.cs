def2405c0b1e630b662b95d6e3aa081b85d7ccc7
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2023-04-24 11:43:00 -0700

    Add files via upload

---
+++ ABSTRACT_CLASS_SUBMISSION/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABSTRACT_CLASS_SUBMISSION
+{
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ ABSTRACT_CLASS_SUBMISSION/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABSTRACT_CLASS_SUBMISSION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.firstName = "Nathan";
+            employee1.lastName = "Ilescas";
+
+            employee1.SayName();
+            Console.ReadKey();
+        }
+    }
+}
