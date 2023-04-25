b77b519590d7a6715531ab2de568befb4170df98
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2023-04-25 09:23:00 -0700

    Add files via upload

---
+++ ABSTRACT_CLASS_SUBMISSION/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABSTRACT_CLASS_SUBMISSION
+{
+    class Employee : Person, IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
