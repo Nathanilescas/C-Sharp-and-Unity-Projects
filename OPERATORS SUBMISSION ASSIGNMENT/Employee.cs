769197a9c35ce10790b54c0e51af21beb8711488
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2023-04-26 11:26:56 -0700

    Add files via upload

---
+++ OPERATORS SUBMISSION ASSIGNMENT/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATORS_SUBMISSION_ASSIGNMENT
+{
+    public class Employee
+    {
+        public int ID;
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.ID == employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if(employee1.ID != employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OPERATORS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATORS_SUBMISSION_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() {ID = 01};
+            Employee employee2 = new Employee() {ID = 02};
+
+            Console.WriteLine((employee1.ID == employee2.ID) ? "Both are equal" : "Not equal");
+            Console.ReadKey();
+        }
+    }
+}
