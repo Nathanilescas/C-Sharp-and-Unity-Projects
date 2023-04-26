c93bf7af2b17d7b597fb7823ba6a3fb36117a8a0
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2023-04-25 21:03:41 -0700

    Add files via upload

---
+++ ABSTRACT_CLASS_SUBMISSION/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABSTRACT_CLASS_SUBMISSION
+{
+    public abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public virtual void SayName()
+        {
+            Console.WriteLine("First Name : " + firstName + "\nLast Name : " + lastName);
+        }
+        
+        
+
+    }
+}
