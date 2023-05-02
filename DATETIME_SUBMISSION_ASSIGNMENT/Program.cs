d6135b854f31cd8cab493f00112d53887eafc708
Nathan C Ilescas <125600285+Nathanilescas@users.noreply.github.com>
2023-05-02 12:26:47 -0700

    Add files via upload

---
+++ DATETIME_SUBMISSION_ASSIGNMENT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DATETIME_SUBMISSION_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime);
+            Console.WriteLine("Input a number of hours.");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("It will be " + currentTime.AddHours(input) + " in " + input + " hours.");
+            Console.ReadKey();
+            
+        }
+    }
+}
